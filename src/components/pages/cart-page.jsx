63426291e633dc036293d733c96509496e24eea1
Andrey Sorokin <andrewsorokin540@gmail.com>
2020-03-06 19:55:46 +0700

    add item options in cart

--- src/components/pages/cart-page.jsx
+++ src/components/pages/cart-page.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class CartPage extends React.Component {
+const CartPage = (props) => {
 
-    render() {
-        const list = this.props.cart.map((item, index) => (
-            <li key={index}>
-                {item.title} {item.count && (item.count)}
-            </li>
-        ))
-        return (
-            <React.Fragment>
-                <h2>Корзина</h2>
-                <div className="catalog">
-                    <ul>
-                        {list}
-                    </ul>
-                </div>
-            </React.Fragment>
-        );
-    }
+    const list = props.cart.map((item, index) => (
+        <li key={index}>
+            {item.title} {item.count && (item.count)}
+            <button onClick={() => props.itemRemoveFromCart(item.id)}>-</button>
+            <button onClick={() => props.allItemsRemoveFromCart(item.id)}>---</button>
+            <button onClick={() => props.addItemToCart(item.id)}>+</button>
+        </li>
+    ))
+    return (
+        <React.Fragment>
+            <h2>Корзина</h2>
+            <div className="catalog">
+                <ul>
+                    {list}
+                </ul>
+            </div>
+        </React.Fragment>
+    );
 }
 
 const mapStateToProps = ({ cart }) => ({ cart })
 
-export default connect(mapStateToProps)(CartPage);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        itemRemoveFromCart: (itemId) => {
+            dispatch({
+                type: 'ITEM_REMOVE_FROM_CART',
+                payload: itemId
+            })
+        },
+        allItemsRemoveFromCart: (itemId) => {
+            dispatch({
+                type: 'ALL_ITEMS_REMOVE_FROM_CART',
+                payload: itemId
+            })
+        },
+        addItemToCart: (itemId) => {
+            dispatch({
+                type: 'ITEM_ADD_TO_CART',
+                payload: itemId
+            })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartPage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -15,36 +15,71 @@ const reducer = (state = initialState, action) => {
             const newItemAlreadyInCartIndex = state.cart.findIndex(item => item.id === action.payload)
             const newItemAlreadyInCart = state.cart.find(item => item.id === action.payload)
             const addedItem = state.dataItems.find(item => item.id === action.payload)
-            console.log(newItemAlreadyInCart)
             if (newItemAlreadyInCart) {
-                console.log(1)
-                const newItemInCart = {
-                    id: addedItem.id,
-                    title: addedItem.title,
-                    count: newItemAlreadyInCart.count + 1
-                }
                 return {
                     ...state,
                     cart: [
                         ...state.cart.slice(0, newItemAlreadyInCartIndex),
-                        newItemInCart,
+                        {
+                            id: addedItem.id,
+                            title: addedItem.title,
+                            count: newItemAlreadyInCart.count + 1
+                        },
                         ...state.cart.slice(newItemAlreadyInCartIndex + 1)
                     ]
                 }
             }
             else {
-                console.log(2)
                 return {
                     ...state,
-                    cart: [...state.cart,
-                    {
-                        id: addedItem.id,
-                        title: addedItem.title,
-                        count: 1
-                    }]
+                    cart: [
+                        ...state.cart,
+                        {
+                            id: addedItem.id,
+                            title: addedItem.title,
+                            count: 1
+                        }
+                    ]
                 }
             }
 
+        case 'ITEM_REMOVE_FROM_CART':
+            const removeItem = state.cart.find(item => item.id === action.payload)
+            const removeItemIndex = state.cart.findIndex(item => item.id === action.payload)
+            console.log('ITEM_REMOVE_FROM_CART', removeItem)
+            if (removeItem.count > 1) {
+                return {
+                    ...state,
+                    cart: [
+                        ...state.cart.slice(0, removeItemIndex),
+                        {
+                            ...removeItem,
+                            count: removeItem.count - 1
+                        },
+                        ...state.cart.slice(removeItemIndex + 1)
+                    ]
+                }
+            }
+            else {
+                return {
+                    ...state,
+                    cart: [
+                        ...state.cart.slice(0, removeItemIndex),
+                        ...state.cart.slice(removeItemIndex + 1)
+                    ]
+                }
+            }
+
+        case 'ALL_ITEMS_REMOVE_FROM_CART':
+            const removeItemsIndex = state.cart.findIndex(item => item.id === action.payload)
+            console.log('ALL_ITEMS_REMOVE_FROM_CART', action.payload)
+            return {
+                ...state,
+                cart: [
+                    ...state.cart.slice(0, removeItemsIndex),
+                    ...state.cart.slice(removeItemsIndex + 1)
+                ]
+            }
 
         default:
             return state;
