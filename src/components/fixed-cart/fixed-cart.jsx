cf569293e9c6a9cc8de4ea9a5aea1bc6ecb90e0a
Andrey Sorokin <andrewsorokin540@gmail.com>
2020-04-20 13:33:42 +0700

    refactor fixed-cart to hooks

--- src/components/fixed-cart/fixed-cart.jsx
+++ src/components/fixed-cart/fixed-cart.jsx
@@ -1,66 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { itemRemoveFromCart, allItemsRemoveFromCart, itemAddToCart } from '../../actions';
 import { sizeNameToRus } from '../../utils';
 import './fixed-cart.scss';
 
-class FixedCart extends React.Component {
+const FixedCart = ({ itemRemoveFromCart, allItemsRemoveFromCart, itemAddToCart, cart }) => {
 
-    state = {
-        cartVisible: false
-    }
+    const [cartVisible, setCartVisible] = useState(false)
 
-    toggle() {
-        this.setState({
-            cartVisible: !this.state.cartVisible
-        })
-    }
-
-    render() {
-
-        const list = this.props.cart.order.map((item, index) => {
-            const { id, title, size, count, total } = item;
-            return (
-                <li key={index} className='fixed-cart__li'>
-                    <div>
-                        <div>
-                            <span className="fixed-cart__item-title">{title}</span>
-                            <span className="fixed-cart__item-size"> ({sizeNameToRus(size)})</span>
-                        </div>
-                        <div className="fixed-cart__number">Кол-во: {count}, сумма: {total}руб.</div>
-                    </div>
-                    <div className='fixed-cart__buttons'>
-                        <div className='fixed-cart__buttons-group'>
-                            <button className='button button-primary' onClick={() => this.props.itemRemoveFromCart(id, size)}>-</button>
-                            <button className='button button-primary' onClick={() => this.props.itemAddToCart(id, 1, size)}>+</button>
-                        </div>
-                        <button className='button button-primary' onClick={() => this.props.allItemsRemoveFromCart(id, size)}>Удалить всё</button>
-                    </div>
-                </li>
-            )
-        })
-
-        const classNames = this.state.cartVisible ? "fixed-cart" : "fixed-cart fixed-cart--hidden"
-        
-        if (this.props.cart.order.length < 1) {
-            return null
-        }
-        
+    const list = cart.order.map((item, index) => {
+        const { id, title, size, count, total } = item;
         return (
-            <div className={classNames}>
-                <div className="fixed-cart__header" onClick={() => this.toggle()}>
-                    <Link to='/cart' className='link-hover-underline' >Корзина ({this.props.cart.total} руб.)</Link>
-                    <div className='fixed-cart__toggler'/>
+            <li key={index} className='fixed-cart__li'>
+                <div>
+                    <div>
+                        <span className="fixed-cart__item-title">{title}</span>
+                        <span className="fixed-cart__item-size"> ({sizeNameToRus(size)})</span>
+                    </div>
+                    <div className="fixed-cart__number">Кол-во: {count}, сумма: {total}руб.</div>
                 </div>
-                <div className="fixed-cart__body">
-                    <ul className='fixed-cart__ul'>
-                        {list}
-                    </ul>
+                <div className='fixed-cart__buttons'>
+                    <div className='fixed-cart__buttons-group'>
+                        <button className='button button-primary' onClick={() => itemRemoveFromCart(id, size)}>-</button>
+                        <button className='button button-primary' onClick={() => itemAddToCart(id, 1, size)}>+</button>
+                    </div>
+                    <button className='button button-primary' onClick={() => allItemsRemoveFromCart(id, size)}>Удалить всё</button>
                 </div>
-            </div>
-        );
+            </li>
+        )
+    })
+
+    const classNames = cartVisible ? "fixed-cart" : "fixed-cart fixed-cart--hidden"
+    
+    if (cart.order.length < 1) {
+        return null
     }
+    
+    return (
+        <div className={classNames}>
+            <div className="fixed-cart__header" onClick={() => setCartVisible(!cartVisible)}>
+                <Link to='/cart' className='link-hover-underline' >Корзина ({cart.total} руб.)</Link>
+                <div className='fixed-cart__toggler'/>
+            </div>
+            <div className="fixed-cart__body">
+                <ul className='fixed-cart__ul'>
+                    {list}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 const mapStateToProps = ({ cart }) => ({ cart })
